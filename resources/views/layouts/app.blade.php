c79ed43e5ed0c0a092a17ec4d20ed442d3e370bd
Darent <darentglusefik601@gmail.com>
2026-01-18 19:50:00 +0700

    Kita coba Deploy ke Vercel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,7 +92,7 @@
     <!-- Footer Start -->
     <div class="container">
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <p class="col-md-4 mb-0 text-body-secondary">© 2024 Ronanda Saputra</p>
+            <p class="col-md-4 mb-0 text-body-secondary">© 2026 Darent Glusefik</p>
 
             <a href="{{ route('home') }}"
                 class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
